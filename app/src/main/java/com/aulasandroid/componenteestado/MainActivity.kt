55919305b4d4c246a4d0a43cd0aee7adeb1ae7eb
SIDNEY CAMPOS ARAGÃO <25132876@dsn127m1246730.cfp127aluno.local>
2026-03-02 16:56:01 -0300

    Adicionando Composable

--- app/src/main/java/com/aulasandroid/componenteestado/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/componenteestado/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aulasandroid.componenteestado
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,11 +14,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Label
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,6 +35,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +53,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComponenteEstadoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   BasicComponentsScreen(modifier = Modifier.padding(innerPadding))
+                   //BasicComponentsScreen(modifier = Modifier.padding(innerPadding))
+                    ComponentesEstadosScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -87,11 +99,57 @@ fun ComponentesEstadosScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val valorNome = remember {
+            mutableStateOf("")
+        }
         TextField(
-            value = "",
-            onValueChange = {}
+            value = valorNome.value,
+            onValueChange = { novoValor ->
+                valorNome.value = novoValor
+            },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words
+            ),
+            placeholder = {
+                Text(text = "Digite seu nome ")
+            },
+            label = {
+                Text(text = "Nome e Sobrenome ")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Icone Pessoa",
+                    tint = Color(8, 68, 166, 255)
+                )
+            }
+        )
+        val valorIdade = remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = valorIdade.value,
+            onValueChange = { novoValor ->
+                valorIdade.value = novoValor
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = {
+                Text(text = "Digite sua idade")
+            },
+            label = {
+                Text(text = "Idade")
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Face,
+                    contentDescription = "Icone Pessoa",
+                    tint = Color(8, 68, 166, 255)
+                )
+
+            }
         )
     }
 }
